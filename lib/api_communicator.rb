98b477389ab128cd9c7bbd1f3ad123aa0e73d238
github username <github email address>
2018-03-13 17:50:32 -0400

    answer

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,29 +2,77 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+def every_character
+ all_characters = []
+
+ (1..9).to_a.each do |number|
+   num_as_string = number.to_s
+   edited_url = "https://www.swapi.co/api/people/?page=" + num_as_string
+   page_string = RestClient.get(edited_url)
+   page_hash = JSON.parse(page_string)
+
+   page_all_characters = page_hash["results"] #array of character hashes for the page
+
+   page_all_characters.each do |character_hash|
+     all_characters << character_hash
+   end
+
+ end
+
+ all_characters
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+
+def get_character_movies_from_api(character)
+ #make the web request
+
+ # iterate over the character hash to find the collection of `films` for the given
+ #   `character`
+ # collect those film API urls, make a web request to each URL to get the info
+ #  for that film
+ # return value of this method should be collection of info about each film.
+ #  i.e. an array of hashes in which each hash reps a given film
+ # this collection will be the argument given to `parse_character_movies`
+ #  and that method will do some nice presentation stuff: puts out a list
+ #  of movies by title. play around with puts out other info about a given film.
+ all_characters = every_character
+
+ films_array = nil
+
+ all_characters.each do |character_hash|
+   if character_hash["name"].downcase == character
+     films_array = character_hash["films"]
+   end
+ end
+
+ movie_array = []
+
+ films_array.each do |movie_link|
+   movie_string = RestClient.get(movie_link)
+   movie_hash = JSON.parse(movie_string)
+
+   movie_array << movie_hash
+ end
+
+ movie_array
+
+ # return an array of hashes, where each hash represents a movie.
+end
+
+
+
+def parse_character_movies(films_hash_array)
+ # some iteration magic and puts out the movies in a nice list
+ puts "***** PRESENT IN *****"
+ films_hash_array.each_with_index do |film, i|
+   puts "#{i+1}. #{film["title"]}"
+ end
+ puts "**********************"
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+ films_hash_array = get_character_movies_from_api(character)
+ parse_character_movies(films_hash_array)
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,44 @@
+require "json"
+require "rest-client"
+require 'pry'
+
 def welcome
-  # puts out a welcome message here!
+puts "**********************"
+puts "Welcome User. You smell nice today."
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+puts "Please enter a character:"
+# use gets to capture the user's input. This method should return that input, downcased.
+user_input = gets.chomp.downcase
+
+until persons_that_exist.map(&:downcase).include?(user_input)
+   puts "That is an invalid character, possible choices:"
+   # iterate thru choices
+   # ask again for input
+   persons_that_exist.each do |person|
+     puts person
+   end
+   puts "******************"
+   puts "Please enter a character:"
+   user_input = gets.chomp.downcase
+
+ end
+
+ user_input.downcase
+end
+
+def persons_that_exist
+ # all_characters = RestClient.get('http://www.swapi.co/api/people/')
+
+ all_characters = every_character
+
+
+ characters_names_array = []
+
+ all_characters.each do |character_hash|
+   characters_names_array << character_hash["name"]
+ end
+
+ characters_names_array.sort
 end
